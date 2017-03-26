e84b2eb9e03df3b018755a8cced99eb9366b487a
Michael Hilus <m.hilus@progresso-group.de>
2017-03-26 22:49:17 +0200

    Added helper classes for id conversion
    Added classes that convert from int and Guid ids to global unique ids and vice versa.

---
+++ src/Nitrolize/Identification/Base64.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Nitrolize.Identification
+{
+    public static class Base64
+    {
+        public static string Encode(string stringToEncode)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(stringToEncode));
+        }
+
+        public static string Decode(string stringToDecode)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(stringToDecode));
+        }
+    }
+}
---
+++ src/Nitrolize/Identification/GlobalId.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace Nitrolize.Identification
+{
+    public static class GlobalId
+    {
+        public static string ToGlobalId(string source, string id)
+        {
+            return Base64.Encode($"{source}#{id}");
+        }
+
+        public static T ToLocalId<T>(string globalId)
+        {
+            return Decompose<T>(globalId).Id;
+        }
+
+        public static object ToLocalId(Type idType, string globalId)
+        {
+            var method = typeof(GlobalId).GetMethods().Where(m => m.Name == "ToLocalId" && m.IsGenericMethod).First();
+            method = method.MakeGenericMethod(idType);
+            return method.Invoke(null, new object[] { globalId });
+        }
+
+        public static string ToEntityName(string globalId)
+        {
+            return Decompose(globalId).Item1;
+        }
+
+        public static Tuple<string, string> Decompose(string globalId)
+        {
+            var decoded = Base64.Decode(globalId);
+            var splitted = decoded.Split('#');
+
+            return new Tuple<string, string>(splitted[0], splitted[1]);
+        }
+
+        public static GlobalIdComponents<T> Decompose<T>(string globalId)
+        {
+            var stringComponents = Decompose(globalId);
+
+            var stringId = Convert.ToString(stringComponents.Item2);
+            var id = GetIdFromString<T>(stringId);
+
+            return new GlobalIdComponents<T>(stringComponents.Item1, id);
+        }
+
+        private static T GetIdFromString<T>(string id)
+        {
+            if (typeof(T) == typeof(Int32))
+            {
+                return (T)(object)Convert.ToInt32(id);
+            }
+
+            if (typeof(T) == typeof(Guid))
+            {
+                return (T)(object)Guid.Parse(id);
+            }
+
+            throw new NotImplementedException($"The type {typeof(T)} as an id type is currently not supported.");
+        }
+
+        public class GlobalIdComponents<T>
+        {
+            public GlobalIdComponents(string entityName, T id)
+            {
+                this.EntityName = entityName;
+                this.Id = id;
+            }
+
+            public string EntityName { get; }
+
+            public T Id { get; }
+        }
+    }
+}
