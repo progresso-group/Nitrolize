101a312933b18f1d80cf78f6b34c6b976aeed28e
Michael Hilus <m.hilus@progresso-group.de>
2017-03-26 22:52:47 +0200

    Added a user context interface
    An IUserContext interface can be used within field validation and within fields for accessing user data like roles etc.

---
+++ src/Nitrolize/Context/IUserContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Nitrolize.Context
+{
+    /// <summary>
+    /// Interface for a user context.
+    /// Can be accessed in fields or before field validation to check for
+    /// authentication or authorization or to deliver user specific data.
+    /// </summary>
+    /// <typeparam name="TId">The type of the user's id.</typeparam>
+    public interface IUserContext<TId>
+    {
+        /// <summary>
+        /// Indicates whether the current user is authenticated.
+        /// </summary>
+        bool IsUserAuthenticated { get; }
+
+        /// <summary>
+        /// Gets the id of the current user.
+        /// </summary>
+        TId UserId { get; }
+
+        /// <summary>
+        /// Gets the name of the current user.
+        /// </summary>
+        string UserName { get; }
+
+        /// <summary>
+        /// Gets the role names of the current user.
+        /// </summary>
+        IEnumerable<string> UserRoles { get; }
+    }
+}
