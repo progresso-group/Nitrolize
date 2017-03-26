db09d9706153c1f1b64f75bb66410b955b5c4821
Michael Hilus <m.hilus@progresso-group.de>
2017-03-26 22:27:07 +0200

    Added solution and project files

---
+++ src/Nitrolize/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("progresso group")]
+[assembly: AssemblyProduct("Nitrolize")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33e0b8bf-e9b7-4343-861c-2ae76a7d238c")]
