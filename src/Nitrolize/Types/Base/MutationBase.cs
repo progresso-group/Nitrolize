c383ddde08f6adfde40f2b5df708fba7b0182ddf
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:48:03 +0200

    Added base types that are later used in Viewers and Mutations

---
+++ src/Nitrolize/Types/Base/MutationBase.cs
@@ -0,0 +1,138 @@
+﻿using GraphQL.Types;
+using Nitrolize.Convenience.Attributes;
+using Nitrolize.Convenience.Delegates;
+using Nitrolize.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Nitrolize.Types.Base
+{
+    /// <summary>
+    /// Base class to be used for application mutation container classes.
+    /// When initialized, converts all annotated properties into Relay compatible 
+    /// mutation fields using the propertie's Delegates as resolving methods.
+    /// </summary>
+    public abstract class MutationBase : ObjectGraphType
+    {
+        /// <summary>
+        /// Initializes a new instance of the mutation container and the
+        /// conversion of the mutation properties.
+        /// </summary>
+        protected MutationBase()
+        {
+            this.FindAndConvertPropertiesToAddMutations();
+            this.FindAndConvertPropertiesToUpdateMutations();
+            this.FindAndConvertPropertiesToDeleteMutations();
+        }
+
+        /// <summary>
+        /// Creates mutation fields for every property found that is annotated
+        /// by the [Mutation] attribute and has the type of <see cref="Add<,>" /> Delegate.
+        /// </summary>
+        private void FindAndConvertPropertiesToAddMutations()
+        {
+            var properties = this.GetMutationPropertiesOfType(typeof(Add<,,>));
+            foreach (var property in properties)
+            {
+                // get the input and entity type
+                var genericArguments = property.PropertyType.GetGenericArguments();
+                var inputType = genericArguments[0];
+                var entityType = genericArguments[1];
+                var viewerType = genericArguments[2];
+
+                // create a resolve Func from the properties Delegate method:
+                // the resolver expects the context and an AddInput as inputs
+                Func<ResolveFieldContext<object>, object, object> resolve = (context, input) =>
+                {
+                    return ((Delegate)(property.GetValue(this))).Method.Invoke(this, new object[] { context, input });
+                };
+
+                // create the field
+                var field = this.CreateAddMutation(inputType, entityType, viewerType, resolve, property.Name.ToFirstLower());
+                this.SetAuthenticationAndAuthorizationConfigForField(field, property);
+            }
+        }
+
+        /// <summary>
+        /// Creates mutation fields for every property found that is annotated
+        /// by the [Mutation] attribute and has the type of <see cref="Update<,>" /> Delegate.
+        /// </summary>
+        private void FindAndConvertPropertiesToUpdateMutations()
+        {
+            var properties = this.GetMutationPropertiesOfType(typeof(Update<,>));
+            foreach (var property in properties)
+            {
+                // get the input and entity type
+                var genericArguments = property.PropertyType.GetGenericArguments();
+                var inputType = genericArguments[0];
+                var entityType = genericArguments[1];
+
+                // create a resolve Func from the properties Delegate method:
+                // the resolver expects the context and an UpdateInput as inputs
+                Func<ResolveFieldContext<object>, object, object> resolve = (context, input) =>
+                {
+                    return ((Delegate)(property.GetValue(this))).Method.Invoke(this, new object[] { context, input });
+                };
+
+                // create the field
+                var field = this.CreateUpdateMutation(inputType, entityType, resolve, property.Name.ToFirstLower());
+                this.SetAuthenticationAndAuthorizationConfigForField(field, property);
+            }
+        }
+
+        /// <summary>
+        /// Creates mutation fields for every property found that is annotated
+        /// by the [Mutation] attribute and has the type of <see cref="Delete<,,>"/> Delegate.
+        /// </summary>
+        private void FindAndConvertPropertiesToDeleteMutations()
+        {
+            var properties = this.GetMutationPropertiesOfType(typeof(Delete<,,>));
+            foreach (var property in properties)
+            {
+                // get the type arguments
+                var genericArguments = property.PropertyType.GetGenericArguments();
+                var idType = genericArguments[0];
+                var entityType = genericArguments[1];
+                var viewerType = genericArguments[2];
+
+                // create a resolve Func from the properties Delegate method
+                // the resolver expects the context, the id and an UpdateInput as inputs
+                Func<ResolveFieldContext<object>, object, object, object> resolve = (context, id, input) =>
+                {
+                    return ((Delegate)(property.GetValue(this))).Method.Invoke(this, new object[] { context, id, input });
+                };
+
+                // create the field
+                var field = this.CreateDeleteMutation(idType, entityType, viewerType, resolve, property.Name.ToFirstLower());
+                this.SetAuthenticationAndAuthorizationConfigForField(field, property);
+            }
+        }
+
+        /// <summary>
+        /// Gets all properties that are annotated by the [Mutation] attribute and
+        /// whose Delegate type matches the given type.
+        /// </summary>
+        /// <param name="type">The Delegate type to search for.</param>
+        /// <returns>All mutation properties with a given Delegate type.</returns>
+        private IEnumerable<PropertyInfo> GetMutationPropertiesOfType(Type type)
+        {
+            var typeName = type.Name.ToCleanGenericTypeDefinitionName();
+
+            var properties = this.GetPropertiesWithAttribute<MutationAttribute>();
+            return properties.Where(p => p.PropertyType.Name.ToCleanGenericTypeDefinitionName().Equals(typeName));
+        }
+
+        /// <summary>
+        /// Sets the authentication and authorization configuration for a given field and property.
+        /// </summary>
+        private void SetAuthenticationAndAuthorizationConfigForField(FieldType field, PropertyInfo property)
+        {
+            var isAuthenticationRequired = property.GetAttribute<MutationAttribute>().IsAuthenticationRequired;
+            var requiredRoles = property.GetRequiredRoles();
+            field.RequiresRoles(requiredRoles);
+            field.RequiresAuthentication(isAuthenticationRequired);
+        }
+    }
+}
---
+++ src/Nitrolize/Types/Base/ViewerTypeBase.cs
@@ -0,0 +1,160 @@
+﻿using GraphQL.Types;
+using Nitrolize.Convenience;
+using Nitrolize.Convenience.Attributes;
+using Nitrolize.Extensions;
+using Nitrolize.Identification;
+using Nitrolize.Types.Node;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Nitrolize.Types.Base
+{
+    /// <summary>
+    /// Base class for a ViewerType.
+    /// Converts all attributed properties of the ViewerType to Connections and Fields automatically.
+    /// </summary>
+    public abstract class ViewerTypeBase : NodeType
+    {
+        protected ViewerTypeBase()
+        {
+            this.FindAndConvertPropertiesToFields();
+            this.FindAndConvertPropertiesToConnections();
+        }
+
+        private void FindAndConvertPropertiesToFields()
+        {
+            // find all field properties
+            var fields = this.GetPropertiesWithAttribute<FieldAttribute>();
+            foreach (var field in fields)
+            {
+                var genericArguments = field.PropertyType.GetGenericArguments();
+
+                // handle Field<TEntity>
+                if (genericArguments.Count() == 1)
+                {
+                    this.ConvertPropertyToSimpleField(field);
+                    continue;
+                }
+
+                // handle Field<TEntity, TId>
+                if (genericArguments.Count() == 2)
+                {
+                    this.ConvertPropertyToGetByIdField(field);
+                    continue;
+                }
+            }
+        }
+
+        private void ConvertPropertyToSimpleField(PropertyInfo field)
+        {
+            var genericArguments = field.PropertyType.GetGenericArguments();
+            var graphType = genericArguments[0].MapToGraphType();
+
+            // get arguments from attributes
+            var arguments = new QueryArguments(field.GetQueryArguments());
+
+            // construct resolving method
+            Func<ResolveFieldContext<object>, object> resolve = (context) =>
+            {
+                return ((Delegate)(field.GetValue(this))).Method.Invoke(this, new object[] { context });
+            };
+
+            // handle authentication and authorization
+            var isAuthenticationRequired = field.GetAttribute<FieldAttribute>().IsAuthenticationRequired;
+            var requiredRoles = field.GetRequiredRoles();
+
+            var graphQLField = this.Field(graphType, field.Name.ToFirstLower(), null, arguments, resolve);
+            graphQLField.RequiresRoles(requiredRoles);
+            graphQLField.RequiresAuthentication(isAuthenticationRequired);
+        }
+
+        private void ConvertPropertyToGetByIdField(PropertyInfo field)
+        {
+            var genericArguments = field.PropertyType.GetGenericArguments();
+            var entityType = genericArguments[0];
+            var idType = genericArguments[1];
+
+            // construct resolving method
+            Func<ResolveFieldContext<object>, object> resolve = (context) =>
+            {
+                // get the connection delegate
+                var method = ((Delegate)(field.GetValue(this))).Method;
+
+                // convert the global id to local id
+                var globalId = context.GetArgument<string>(entityType.GetIdPropertyName().ToFirstLower());
+                var id = GlobalId.ToLocalId(idType, globalId);
+
+                // invoke field with "context" and "id"
+                return ((Delegate)(field.GetValue(this))).Method.Invoke(this, new object[] { context, id });
+            };
+
+            // handle authentication and authorization
+            var isAuthenticationRequired = field.GetAttribute<FieldAttribute>().IsAuthenticationRequired;
+            var requiredRoles = field.GetRequiredRoles();
+
+            var graphQLField = this.AddSingleField(entityType, resolve, field.Name.ToFirstLower());
+            graphQLField.RequiresRoles(requiredRoles);
+            graphQLField.RequiresAuthentication(isAuthenticationRequired);
+        }
+
+        private void FindAndConvertPropertiesToConnections()
+        {
+            // find all connection properties
+            var connections = this.GetPropertiesWithAttribute<ConnectionAttribute>();
+            foreach (var connection in connections)
+            {
+                var entityType = connection.PropertyType.GetGenericArguments()[0];
+
+                // get arguments from attributes
+                var arguments = connection.GetQueryArguments();
+
+                // construct resolving method
+                Func<ResolveFieldContext<object>, object> resolve = (context) =>
+                {
+                    // get the connection delegate
+                    var method = ((Delegate)(connection.GetValue(this))).Method;
+
+                    // get order by infos
+                    var orderByValue = context.GetArgument<int?>("orderBy");
+                    var orderByPropertyName = string.Empty;
+                    var orderAsc = false;
+
+                    if (orderByValue != null)
+                    {
+                        var additionsType = Extensions.TypeExtensions.CreateAdditionsTypeForType(entityType, arguments.Select(a => a.Name).ToArray());
+                        var orderByType = typeof(OrderByType<,>).MakeGenericType(entityType, additionsType);
+                        var orderBy = (EnumerationGraphType)Activator.CreateInstance(orderByType, new object[] { });
+
+                        var orderByIdentifier = orderBy.Values.First(v => (int)v.Value == orderByValue.Value);
+                        orderByPropertyName = orderByIdentifier.Name.Split('_')[0];
+                        orderAsc = orderByIdentifier.Name.Split('_')[1] == "ASC";
+                    }
+
+                    // construct parameters
+                    var parametersType = typeof(Parameters);
+                    var parameters = Activator.CreateInstance(parametersType, new object[]
+                    {
+                            context.GetArgument<string>("after"),
+                            context.GetArgument<int?>("first"),
+                            context.GetArgument<string>("before"),
+                            context.GetArgument<int?>("last"),
+                            orderByPropertyName,
+                            orderAsc
+                    });
+
+                    // invoke connection with "context" and "parameters"
+                    return ((Delegate)(connection.GetValue(this))).Method.Invoke(this, new object[] { context, parameters });
+                };
+
+                // handle authentication and authorization
+                var isAuthenticationRequired = connection.GetAttribute<ConnectionAttribute>().IsAuthenticationRequired;
+                var requiredRoles = connection.GetRequiredRoles();
+
+                var graphQLField = this.AddConnectionField(entityType, resolve, connection.Name.ToFirstLower(), arguments.ToArray());
+                graphQLField.RequiresRoles(requiredRoles);
+                graphQLField.RequiresAuthentication(isAuthenticationRequired);
+            }
+        }
+    }
+}
