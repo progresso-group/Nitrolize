6ca4d1ec6070fc04e74109ede854eed80c7659e8
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:34:45 +0200

    Added string extensions for name conversions

---
+++ src/Nitrolize/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Nitrolize.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts a string by lowering it's first character.
+        /// I.e.: "SomeString" => "someString"
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ToFirstLower(this string input)
+        {
+            return char.ToLowerInvariant(input[0]) + input.Substring(1);
+        }
+
+        /// <summary>
+        /// Converts a generic type definition name to a cleaner readable name.
+        /// I.e.: "List`1" => List
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string ToCleanGenericTypeDefinitionName(this string name)
+        {
+            return name.Remove(name.IndexOf('`'));
+        }
+    }
+}
