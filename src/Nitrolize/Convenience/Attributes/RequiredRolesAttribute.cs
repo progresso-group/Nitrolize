f4bf196a229f659d32034d39263c11fc9d73458b
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:38:09 +0200

    Added an attribute for authorization and roles check

---
+++ src/Nitrolize/Convenience/Attributes/RequiredRolesAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nitrolize.Convenience.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredRolesAttribute : Attribute
+    {
+        public RequiredRolesAttribute(params string[] requiredRoles)
+        {
+            this.RequiredRoles = requiredRoles;
+        }
+
+        public string[] RequiredRoles { get; }
+    }
+}
