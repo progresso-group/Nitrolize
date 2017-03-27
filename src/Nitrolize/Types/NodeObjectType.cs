de6fb18e77e11f569cdd2c8447d0985d7352090a
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:46:39 +0200

    Added types for Objects and NodeObjects (those with Ids)

---
+++ src/Nitrolize/Types/NodeObjectType.cs
@@ -0,0 +1,14 @@
+﻿using Nitrolize.Extensions;
+using Nitrolize.Interfaces;
+using Nitrolize.Types.Node;
+
+namespace Nitrolize.Types
+{
+    public class NodeObjectType<T> : NodeType<T>, IGraphType<T> where T : class
+    {
+        public NodeObjectType() : base()
+        {
+            this.AddFieldsFromType<T>();
+        }
+    }
+}
---
+++ src/Nitrolize/Types/ObjectType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using Nitrolize.Extensions;
+
+namespace Nitrolize.Types
+{
+    public class ObjectType<T> : ObjectGraphType where T : class
+    {
+        public ObjectType() : base()
+        {
+            this.Name = typeof(T).Name;
+            this.IsTypeOf = value => value is T;
+
+            this.AddFieldsFromType<T>(handleIdProperty: true);
+        }
+    }
+}
