4efbdbeb97d74cf6123a730f8e7faeb0c57f28f8
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:38:52 +0200

    Added tests for string extensions

---
+++ src/Nitrolize.Tests/StringExtensionsSpecification.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Machine.Fakes;
+using Machine.Specifications;
+using Nitrolize.Extensions;
+
+namespace Nitrolize.Tests
+{
+    [Subject(typeof(StringExtensions))]
+    public class StringExtensionsSpecification : WithSubject<string>
+    {
+    }
+
+    public class When_converting_to_first_lower : StringExtensionsSpecification
+    {
+        protected static string Result;
+
+        Establish context = () => Result = "SomeExampleString";
+
+        Because of = () => Result = Result.ToFirstLower();
+
+        It should_have_set_first_character_to_lower_case = () => Result[0].Should().Be('s');
+    }
+}
