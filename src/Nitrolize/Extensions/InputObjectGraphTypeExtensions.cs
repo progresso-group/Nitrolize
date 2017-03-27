0ac22765d006c3343205f0c711dd03a882c5265e
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:53:43 +0200

    Added input object graph type extensions for generating input types from models

---
+++ src/Nitrolize/Extensions/InputObjectGraphTypeExtensions.cs
@@ -0,0 +1,53 @@
+﻿using GraphQL.Types;
+using Nitrolize.Types.Input;
+
+namespace Nitrolize.Extensions
+{
+    public static class InputObjectGraphTypeExtensions
+    {
+        public static void AddFieldsFromType<T>(this InputObjectGraphType type, bool omitIdProperty)
+        {
+            var properties = typeof(T).GetExactProperies();
+            foreach (var property in properties)
+            {
+                var typeName = property.PropertyType.Name;
+                var propertyName = property.Name.ToFirstLower();
+
+                // create IdGraphType field for Id property
+                if (propertyName == "id")
+                {
+                    // skip id property if desired
+                    if (omitIdProperty)
+                    {
+                        continue;
+                    }
+
+                    type.Field<IdGraphType>("id", "The global unique id of an object");
+                    continue;
+                }
+
+                // create primitive fields like ints, strings, floats etc.
+                if (property.PropertyType.IsSimpleType())
+                {
+                    type.CreateFieldFromPrimitiveProperty(property);
+                    continue;
+                }
+
+                // generate list field for collection / list
+                if (property.PropertyType.GetInterface("ICollection") != null && property.PropertyType.IsGenericType)
+                {
+                    // create an InputType<itemType>
+                    var itemType = property.PropertyType.GenericTypeArguments[0];
+                    var graphQLType = typeof(InputType<>);
+                    graphQLType = graphQLType.MakeGenericType(itemType);
+                    graphQLType = graphQLType.ConvertToVirtualType();
+
+                    // call AddListField like type.AddListField<Order, InputType<Order>>("orders")
+                    var method = typeof(ComplexGraphTypeExtensions).GetMethod("AddListField");
+                    method = method.MakeGenericMethod(new[] { itemType, graphQLType });
+                    method.Invoke(type, new object[] { type, propertyName });
+                }
+            }
+        }
+    }
+}
