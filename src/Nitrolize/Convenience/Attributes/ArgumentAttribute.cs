3ce0cc8635dc8de2ba897fb088c59770df3f4ae7
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:55:44 +0200

    Added attributes for arguments and mutations

---
+++ src/Nitrolize/Convenience/Attributes/ArgumentAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Nitrolize.Extensions;
+using System;
+
+namespace Nitrolize.Convenience.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ArgumentAttribute : Attribute
+    {
+        public ArgumentAttribute(string name, Type argumentType)
+        {
+            this.ArgumentType = argumentType.MapToGraphType();
+            this.Name = name;
+        }
+
+        public Type ArgumentType { get; }
+
+        public string Name { get; }
+    }
+}
