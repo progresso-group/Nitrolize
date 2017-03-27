b93cd71d86911545272b1f0cf83a25caafaeb8cd
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:46:09 +0200

    Added a structure for GraphQL queries

---
+++ src/Nitrolize/Web/GraphQLQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Nitrolize.Web
+{
+    public class GraphQLQuery
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public Dictionary<string, object> Variables { get; set; }
+    }
+}
