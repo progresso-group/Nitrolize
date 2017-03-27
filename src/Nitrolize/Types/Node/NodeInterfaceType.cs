468d2f4c1d08c18a47123870219198338a6c6d56
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:45:19 +0200

    Added the Node type and interface type

---
+++ src/Nitrolize/Types/Node/NodeInterfaceType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace Nitrolize.Types.Node
+{
+    public class NodeInterfaceType : InterfaceGraphType
+    {
+        public NodeInterfaceType()
+        {
+            this.Name = "Node";
+            this.Field<NonNullGraphType<IdGraphType>>("id", "The id of the node");
+        }
+    }
+}
---
+++ src/Nitrolize/Types/Node/NodeType.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using Nitrolize.Extensions;
+
+namespace Nitrolize.Types.Node
+{
+    public class NodeType : ObjectGraphType
+    {
+        public NodeType()
+        {
+            this.Interface<NodeInterfaceType>();
+            this.AddIdField();
+            this.IsTypeOf = value => true;
+        }
+    }
+
+    public class NodeType<T> : NodeType where T : class
+    {
+        public NodeType()
+        {
+            this.Name = typeof(T).Name;
+            this.IsTypeOf = value => value is T;
+        }
+    }
+}
