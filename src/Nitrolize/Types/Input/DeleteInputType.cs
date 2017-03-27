1bce6ed69aa40ef261263a8a25679e6e763fcf9b
Michael Hilus <m.hilus@progresso-group.de>
2017-03-28 01:45:36 +0200

    Added the input type for deletions

---
+++ src/Nitrolize/Types/Input/DeleteInputType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+
+namespace Nitrolize.Types.Input
+{
+    public class DeleteInputType<T> : InputObjectGraphType where T : class
+    {
+        public DeleteInputType()
+        {
+            var typeName = typeof(T).Name;
+            this.Name = $"delete{typeName}Input";
+
+            this.Field<NonNullGraphType<IdGraphType>>("id");
+            this.Field<StringGraphType>("clientMutationId");
+        }
+    }
+}
